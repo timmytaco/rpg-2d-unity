c02bacbf5e4dd06b95772533642516dc65324f21
timmytaco <heath.d.atkins@gmail.com>
2024-02-12 22:29:13 -0500

    Add files via upload

---
+++ Assets/Rotating.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Rotating : MonoBehaviour
+{
+    private Transform aimTransform;
+    
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
+    }
+}
